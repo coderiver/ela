d0b1e3853ef3839dcb9c09919e771f971f84a80a
Eugene Godun <eugene.godun@gmail.com>
2015-05-18 17:42:44 +0300

    index

--- js/common.js
+++ js/common.js
@@ -15,5 +15,31 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	console.log($('body').html());
+	$(".js-toggle-menu").on("click", function(){
+		$(this).toggleClass("is-active");
+		$(".js-menu").toggleClass("is-active");
+		return false;
+	});
+	$(".js-nav-main a[data-nav]").on("click", function(){
+		var nav = $("."+$(this).attr("data-nav"));
+		$(".js-nav-main").fadeOut(200);
+		nav.fadeIn(200);
+		$(this).toggleClass("is-active");
+		return false;
+	});
+	$(".js-nav-back").on("click", function(){
+		$(".js-nav").fadeOut(200);
+		$(".js-nav-main").fadeIn(200);
+		return false;
+	});
+
+	var slickSet = {
+		slideToShow: 1,
+		slideToScroll: 1,
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		mobileFirst: true
+	};
+	$(".js-slider").slick(slickSet);
 });
