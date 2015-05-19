33606a8886e4704b732ad95900145014b2b12363
Eugene Godun <eugene.godun@gmail.com>
2015-05-19 17:47:43 +0300

    popup

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$(document).on("click", function(){
+		$(".js-menu").removeClass("is-active");
+		$("body").removeClass("no-scroll");
+		$(".js-popup").removeClass("is-visible");
+	});
 
 	// function scrollFixedElements() {
 	//     var scroll_left = $(this).scrollLeft();
@@ -15,11 +17,14 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	$(".js-toggle-menu").on("click", function(){
-		$(this).toggleClass("is-active");
+	$(".js-toggle-menu").on("click", function(event){
 		$(".js-menu").toggleClass("is-active");
-		$("body").toggleClass("has-open-menu");
+		$("body").toggleClass("no-scroll");
+		event.stopPropagation();
 		return false;
+	});
+	$(".js-menu").on("click", function(event){
+		event.stopPropagation();
 	});
 	$(".js-nav-main a[data-nav]").on("click", function(){
 		var nav = $("."+$(this).attr("data-nav"));
@@ -43,4 +48,36 @@ head.ready(function() {
 		mobileFirst: true
 	};
 	$(".js-slider").slick(slickSet);
+
+	$(".js-popup").on("click", function(event){
+		event.stopPropagation();
+	});
+
+// open popup
+	$(".js-toggle-popup").on("click", function(event){
+		var popup = $("." + $(this).attr("data-popup"));
+		popup.toggleClass("is-visible");
+		event.stopPropagation();
+		return false;
+	});
+    $(".js-popup").on("click", function(event){
+        event.stopPropagation();
+    });
+
+
+	$(".js-clear-input").on("click", function(){
+		$(this).parents(".js-form").find(".js-input").val("");
+		$(this).fadeOut();
+		return false;
+	});
+
+	$(".js-input").on("keyup", function(){
+		if ($(this).val().length) {
+			$(this).parents(".js-form").find(".js-clear-input").fadeIn();
+		}
+		else {
+			$(this).parents(".js-form").find(".js-clear-input").fadeOut();
+		}
+		
+	});
 });
